21b3e1c56158bec055df95bec0b9dc3145a8fe9a
Nina Böhm <nina.boehm@climatepartner.com>
2022-06-23 15:51:05 +0200

    update dependencies and fix tests

--- certificate-service/src/certificate.controller.spec.ts
+++ certificate-service/src/certificate.controller.spec.ts
@@ -8,21 +8,21 @@ describe('CertificateController', () => {
     let userClient: ClientKafka;
     let certificates: any[] = [
         {
-            certificateId: '07ee9478-2b06-4581-9906-b1325e68885d',
+            certificateId: '1337',
             sizeHectare: 40,
             storagePotential: 0.4,
             carbonStorageTonnes: 4800,
             userId: null
         },
         {
-            certificateId: '8cf715f7-af4a-4805-9341-d4013a4a028f',
+            certificateId: '1338',
             sizeHectare: 20,
             storagePotential: 0.9,
             carbonStorageTonnes: 5400,
             userId: null
         },
         {
-            certificateId: '6c123d0c-0e3b-4637-9376-361189097762',
+            certificateId: '1339',
             sizeHectare: 200,
             storagePotential: 0.1,
             carbonStorageTonnes: 6000,
@@ -52,10 +52,10 @@ describe('CertificateController', () => {
 
     describe('assignCertificate', () => {
         it('should assign a userId to a certificate', async () => {
-            const certificateId = '07ee9478-2b06-4581-9906-b1325e68885d';
+            const certificateId = '1337';
             const userId = '123'
             const expected = {
-                certificateId: '07ee9478-2b06-4581-9906-b1325e68885d',
+                certificateId: '1337',
                 sizeHectare: 40,
                 storagePotential: 0.4,
                 carbonStorageTonnes: 4800,
@@ -66,8 +66,8 @@ describe('CertificateController', () => {
         })
 
         it('should return the index of the correct certificate', async () => {
-            const certificateId = '8cf715f7-af4a-4805-9341-d4013a4a028f';
-            expect(certificateService.identifyCertificate(certificates, certificateId)).toBe(1);
+            const certificateId = '1337';
+            expect(certificateService.identifyCertificate(certificates, certificateId)).toBe(0);
         })
     })
 })
