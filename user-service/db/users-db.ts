f5550e444fcd18cf5360233a2e6a39efea843996
ninabohm <nina.boehm1994@gmail.com>
2022-07-04 10:13:44 +0200

    feature: users in separate db file

---
+++ user-service/db/users-db.ts
@@ -0,0 +1,12 @@
+let users;
+export default users = [
+    {
+        "userId": "123",
+        "stripeUserId": "89dfcfd0-3422-4a0d-9afd-8886a8a068df",
+        "name": "Alice"
+    },
+    {
+        "userId": "345",
+        "stripeUserId": "0de8700b-2f80-4f20-8565-a14659236db1",
+        "name": "Bob"}
+]
--- user-service/src/app.service.ts
+++ user-service/src/app.service.ts
@@ -1,23 +1,13 @@
 import {Injectable} from '@nestjs/common';
 import {RetrieveUserRequest} from "./retrieve-user-request.dto";
+import users from '../db/users-db';
 
 @Injectable()
 export class AppService {
-  private readonly users: any[] = [
-    {
-      userId: '123',
-      stripeUserId: '89dfcfd0-3422-4a0d-9afd-8886a8a068df',
-      name: 'Alice'
-    },
-    {
-      userId: '345',
-      stripeUserId: '0de8700b-2f80-4f20-8565-a14659236db1',
-      name: 'Bob'
-    },
-  ]
+
 
   retrieveUser(retrieveUserRequest: RetrieveUserRequest) {
     console.log("identifying the correct user");
-    return this.users.find((user) => user.userId === retrieveUserRequest.userId);
+    return users.find((user) => user.userId === retrieveUserRequest.userId);
   }
 }
